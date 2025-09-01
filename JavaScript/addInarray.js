8dfef3418147c233a9651cbfe72900e542499020
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-09-01 19:21:54 +0530

    js

---
+++ JavaScript/addInarray.js
@@ -0,0 +1,15 @@
+let arr =[11,22,33,44,55,66]
+// add and delete from back
+arr.push(77)
+console.log(arr);
+arr.pop()
+console.log(arr);
+
+// add and delete from front
+arr.unshift(11)
+console.log(arr);
+arr.shift(11)
+console.log(arr);
+
+
+
