fa4eab5252ccdc2dd2ed3c7fe3bd3399d6173e20
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-09-09 10:30:08 +0530

    Dom

---
+++ Dom/script.js
@@ -0,0 +1,19 @@
+// let h1 = document.getElementById("one")
+// console.log(h1);
+
+// h1.style.color='red'
+
+let h2 = document.getElementsByClassName("two")
+console.log(h2);
+
+for(let i of h2){
+    i.style.color='blue'
+}
+
+// let h3 = document.querySelector("#one")
+// console.log(h3);
+
+// let h4 = document.querySelectorAll(".two")
+// console.log(h4);
+
+
