b2d46f615fd5164045611bc6346fada3d4ab69a4
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-09-09 10:49:11 +0530

    Dom

--- Dom/script.js
+++ Dom/script.js
@@ -1,7 +1,7 @@
-// let h1 = document.getElementById("one")
-// console.log(h1);
+let h1 = document.getElementById("one")
+console.log(h1);
+h1.style.color='red'
 
-// h1.style.color='red'
 
 let h2 = document.getElementsByClassName("two")
 console.log(h2);
@@ -10,10 +10,19 @@ for(let i of h2){
     i.style.color='blue'
 }
 
-// let h3 = document.querySelector("#one")
-// console.log(h3);
+h2[1].style.color='grey'
+
+
+let h3 = document.querySelector(".four")
+console.log(h3.innerText="hiii");
+h3.style.color='green'
+
 
 // let h4 = document.querySelectorAll(".two")
 // console.log(h4);
+
+let a = document.querySelector("a")
+a.setAttribute("href","http://flipkart.com")
+console.log(a.getAttribute("href"));
 
 
